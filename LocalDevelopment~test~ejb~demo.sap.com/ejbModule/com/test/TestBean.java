14c9d1e768c27b1a4591f11a95630129211b1205
Venkateshpusarla <venkatesh.p.pusarla@gmail.com>
2017-03-31 15:40:46 +0530

    local
    Change-Id: Ia7ba01c64d6155b6fc50437f5fc00c937dede9b7

---
+++ LocalDevelopment~test~ejb~demo.sap.com/ejbModule/com/test/TestBean.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class TestBean
+ */
+@Stateless
+public class TestBean implements TestBeanLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public TestBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
